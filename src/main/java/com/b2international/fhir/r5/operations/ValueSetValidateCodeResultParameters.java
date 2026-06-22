c91e5be9eafebbdde9fc728b5db5b0458b076ede
Adam Filep <adam.filep@iqvia.com>
2026-06-22 12:03:30 +0200

    chore(valuset.validation): fix typo

--- src/main/java/com/b2international/fhir/r5/operations/ValueSetValidateCodeResultParameters.java
+++ src/main/java/com/b2international/fhir/r5/operations/ValueSetValidateCodeResultParameters.java
@@ -95,10 +95,10 @@ public class ValueSetValidateCodeResultParameters extends BaseParameters {
 	}
 	
 	public ValueSetValidateCodeResultParameters setSystem(String system) {
-		return setSysytem(new UriType(system));
+		return setSystem(new UriType(system));
 	}
 
-	public ValueSetValidateCodeResultParameters setSysytem(UriType uriType) {
+	public ValueSetValidateCodeResultParameters setSystem(UriType uriType) {
 		addParameter("system", uriType);
 		return this;
 	}
